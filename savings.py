1a6a54df22d218b573aac2809d88cdb367480b2d
Alejandro Veloz <aleemrtinez@gmail.com>
2023-05-21 10:08:00 -0400

    UPDATE
    This code is now able to show places nearby, using my API_KEY. 
    It seems to be working fine with the googlemaps library. 
    
    Still missing: 
    - Getting the code to get the information I want from each store website. 
    - Show best deals for the customer.

--- savings.py
+++ savings.py
@@ -2,83 +2,77 @@
 #DESCRIPTION: This will be an app that will 
 #users to find best grocery deals around them. 
 #they will be able to save money by just entering their zip code.
-from geopy.geocoders import Nominatim
-import requests
+#from geopy.geocoders import Nominatim
+import googlemaps
+import time
 from bs4 import BeautifulSoup
+from geopy.geocoders import Nominatim
 from config import API_KEY
+import googlemaps.places as places
+
+
+# Verify with Google so we can use my API-Key
+map_client = googlemaps.Client(API_KEY)
 
 # Initialize the geolocator object
-geolocator = Nominatim(user_agent='my_app')
+geolocator = Nominatim(user_agent='my_savings_app')
 
 # Get the zip code from the user
 zipcode = input("Please enter your Zip Code: ")
 
-# Get the distance from the user
-distance = int(input("How many miles away would you like to see your deals?"))
-
-# Find the user's location using the geolocator object
+# Find user's location using geolocator.
 location = geolocator.geocode(f"{zipcode}, USA")
 
-# Print the user's location
-print("It looks like your location is:", location.address)
-
-# Get the latitude and longitude of the user's location
+# Get the latitude and longitude for the parameters
 latitude = location.latitude
 longitude = location.longitude
 
-# Find grocery stores near the user's location
-grocery_stores = []
-search_query = "Supermarket"
-url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={distance}&type={search_query}&key={API_KEY}"
-#print("FLAG 1")
-response = requests.get(url)
-#print("FLAG 2")
-if response.status_code == 200:
-    #print("FLAG 3")
-    results = response.json()["results"]
-    #print("FLAG 4")
-    for search_result in results:
-    #    print("FLAG 5")
-        grocery_stores.append(search_result["name"])
-    #    print("FLAG 6")
-else:
-    print("Your search was not possible...")
+# Get the distance from the user
+distance = int(input("How many miles away would you like to see your deals? "))
 
-# Debugging step to check the grocery stores list
-#print(grocery_stores)
-print(grocery_stores)
-# Scrape pricing data from grocery stores for specific items
-print("FLAG 7")
-#grocery_items = ["apples", "bread", "milk", "chocolate"]
-#grocery_data = {}
-#for store in grocery_stores:
- #  grocery_data[store] = {}
- #   for item in grocery_items:
-#        #url = f"https://www.{store}.com/search?q={item}" #this URL is giving me an error. NEED TO FIX
-#        print(f"{store} and {item}")#Store is getting "Cedar Bluff" which is a location not a store. 
-#        response = requests.get(url)
-#        if response.status_code == 200:
-#            soup = BeautifulSoup(response.text, "html.parser")
-#            if soup.find("span", class_="price") is not None:
-#                price = soup.find("span", class_="price").text
-#                grocery_data[store][item] = price
-#            else:
-#                print(f"{item} not found at {store}")
-#        else:
-#            print(f"Unable to retrieve data for {store} - {item}")
+# Define our search
+places_result = map_client.places_nearby(location=f"{latitude},{longitude}", radius=distance, open_now=False, type='supermarket')
+
+# Loop through each place in the result
+for place in places_result['results']:
+
+    # Defining my place ID
+    my_place_id = place['place_id']
+
+    # Defining what information I would like to gather
+    my_fields = ['name', 'formatted_phone_number', 'type']
+
+    # Make a request for the details
+    place_details = places.place(client=map_client, place_id=my_place_id, fields=my_fields)
+
+    # Print the name of the place
+    print(place_details['result']['name'])
+
+#print(places_result)
+
+#to make sure it works we need to make sure the script is paused. 
+#time.sleep(3)
+
+#get the next 20 results.
+#places_result = map_client.places_nearby(page_token = places_result['next_page_token'])
+
+# Get the distance from the user.
+#distance = input('Enter the distance in meters : ')
 #
-# Find the best deals for each item
-#deals = {}
-#for item in grocery_items:
-#    best_price = float("inf")
-#    best_store = ""
-#    for store, data in grocery_data.items():
-#        if item in data and float(data[item]) < best_price:
-#            best_price = float(data[item])
-#            best_store = store
-#    if best_store:
-#        deals[item] = (best_store, best_price)
-# Print out the best deals for each item
-#for item, (store, price) in deals.items():
-#    print(f"The best deal for {item} is at {store} for ${price:.2f}")
-#
+# Find the user's location using the geolocator object
+#location = geolocator.geocode(f"{zipcode}, USA")
+
+# Get the latitude and longitude of the user's location
+#latitude = location.latitude
+#longitude = location.longitude
+
+# Specify what's the search query you would like to use:
+#search_query = 'Grocery Store'
+
+# Create a list of the businesses.
+#business_list = []
+
+ 
+
+#I found the error in the main program. My API is not working properly. 
+# need to find a way to set it up correctly.
